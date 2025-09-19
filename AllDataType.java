c364e3431862c1c0aef64bc25bc839273dc5049a
Tushar Upadhyay <tusharupadhyay690@gmail.com>
2025-09-19 17:32:36 +0530

    literals

---
+++ AllDataType.java
@@ -0,0 +1,16 @@
+public class AllDataType {
+
+    public static void main(String[] args) {
+        int num1 = 10;
+        byte b1 = 127;
+        float f1 = 10.01f;
+        char ch = 'a';
+        double d2 = 10.11;
+
+        long l1 = 111111111111l;
+
+        boolean bool1 = num1 == l1;
+        boolean bool2 = true;
+        System.out.println(bool1);
+    }
+}
---
+++ Literals.java
@@ -0,0 +1,13 @@
+public class Literals {
+
+    public static void main(String[] args) {
+        int count = 827;
+        int octa = 067;
+        int hexa = 0x67e;
+        int binary = 0b1101;
+        // System.out.println(count);
+
+
+        System.out.println(binary);
+    }
+}
