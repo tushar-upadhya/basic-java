3ee37a24df1679ec7fae3bda291257e0bdc107f1
Tushar Upadhyay <tusharupadhyay690@gmail.com>
2025-09-19 16:55:53 +0530

    data-types

---
+++ DataType.java
@@ -0,0 +1,14 @@
+public class DataType {
+
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 20;
+        int result = a + b;
+
+        char c = 'v';
+        System.out.println(c);
+
+        String name = "tushar";
+        System.out.println(name);
+    }
+}
